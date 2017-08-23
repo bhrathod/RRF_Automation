7a8b6fdc372b2e2483dd25c9d56be1c781670a7c
bhrathod <bharat.2301@gmail.com>
2017-08-23 16:07:32 +0530

    First Migration Commit

---
+++ RRF_Automation/Features/Feature1/Feature1.feature
@@ -0,0 +1,9 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="85214b81-9f85-47ba-935f-2df46093fb2a" description="SharePoint App Feature" featureId="85214b81-9f85-47ba-935f-2df46093fb2a" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="RRF_Automation Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="f035e15b-f0bb-43ed-9b62-33e539477337" />
+    <projectItemReference itemId="745aa822-230d-4ee3-88a8-ed569660a8cf" />
+    <projectItemReference itemId="a62deddd-674a-491d-90b4-ecd994375ed0" />
+    <projectItemReference itemId="abe7522f-186a-4b10-acd0-ee70ddc48f48" />
+  </projectItems>
+</feature>
---
+++ RRF_Automation/pkg/Debug/RRF_Automation/RRF_Automation_Feature1/Scripts/RRFScript.js
@@ -0,0 +1,120 @@
+﻿var collListItems = null;
+
+$(document).ready(function () {
+    ExecuteOrDelayUntilScriptLoaded(retrieveListItems, "sp.js");
+});
+function retrieveListItems() {
+    var hostweburl = decodeURIComponent(getQueryStringParameter("SPHostUrl"));
+    var appweburl = decodeURIComponent(getQueryStringParameter("SPAppWebUrl"));
+    //var clientContext = new SP.ClientContext(siteUrl);
+    //var factory = new SP.ProxyWebRequestExecutorFactory(siteUrl);
+    //clientContext.set_webRequestExecutorFactory(factory);
+    var clientContext;
+    var factory;
+    var appContextSite;
+    var collList;
+    clientContext = new SP.ClientContext(appweburl);
+    //factory = new SP.ProxyWebRequestExecutorFactory(appweburl);
+    //clientContext.set_webRequestExecutorFactory(factory);
+    appContextSite = new SP.AppContextSite(clientContext, hostweburl);
+    var hostweb = appContextSite.get_web();
+
+    //var hostweb = clientContext.get_web();
+    var oList = hostweb.get_lists().getByTitle('Currency');
+
+    var camlQuery = new SP.CamlQuery();
+    camlQuery.set_viewXml('<View><Query><Where></Where></Query></View>');
+    this.collListItem = oList.getItems(camlQuery);
+
+    clientContext.load(collListItem);
+
+    clientContext.executeQueryAsync(Function.createDelegate(this, this.onQuerySucceeded), Function.createDelegate(this, this.onQueryFailed));
+
+}
+
+function onQuerySucceeded(sender, args) {
+
+    var listItemEnumerator = collListItem.getEnumerator();
+        var financialYear = null;
+        var currencyName = null;
+        var inr = null;
+        //var currency = {
+        //    "Financial Year": financialYear,
+        //    "Currency Name": currencyName,
+        //    "INR" : inr
+        //}
+        //var currencyList = [];
+
+        while (listItemEnumerator.moveNext()) {
+            var oListItem = listItemEnumerator.get_current();
+            financialYear = oListItem.get_item("FinancialYear");
+            currencyName = oListItem.get_item("CurrencyName");
+            inr = oListItem.get_item("INR");
+            var curName = document.getElementById('ddl_currencyName');
+            var option = document.createElement('option');
+            option.text = currencyName;
+            option.value = currencyName;
+            curName.add(option);
+        }
+}
+
+function onQueryFailed(sender, args) {
+
+    alert('Request failed. ' + args.get_message() + '\n' + args.get_stackTrace());
+}
+
+//function retrieveListItems1() {
+//    var siteUrl = decodeURIComponent(getQueryStringParameter("SPHostUrl"));    
+//    var clientContext = new SP.ClientContext(siteUrl);
+//    var oList = clientContext.get_web().get_lists().getByTitle('Currency');
+        
+//    var camlQuery = new SP.CamlQuery();
+//    camlQuery.set_viewXml('<View><Query><Where></Where></Query></View>');
+//    collListItems = oList.getItems(camlQuery);
+        
+//    clientContext.load(collListItems);
+        
+//    clientContext.executeQueryAsync(retrieveSuccess, retrieveFail);        
+//    return false;
+//}
+
+//function retrieveSuccess(sender, args) {
+//    var listItemEnumerator = collListItems.getEnumerator();
+//    var financialYear = null;
+//    var currencyName = null;
+//    var inr = null;
+//    var currency = {
+//        "Financial Year": financialYear,
+//        "Currency Name": currencyName,
+//        "INR" : inr
+//    }
+//    var currencyList = [];
+        
+//    while (listItemEnumerator.moveNext()) {
+//        var oListItem = listItemEnumerator.get_current();
+//        financialYear = oListItem["FinancialYear"];
+//        currencyName = oListItem["CurrencyName"];
+//        inr = oListItem["INR"];
+//        var curName = document.getElementById('ddl_currencyName');
+//        var option = document.createElement('option');
+//        option.text = currencyName;
+//        option.value = currencyName;
+//        curName.add(option);
+//    }
+
+//}
+
+//function retrieveFail(sender, args) {
+
+//    alert('Request failed. ' + args.get_message() + '\n' + args.get_stackTrace());
+//}
+
+function getQueryStringParameter(name) {
+    var url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
